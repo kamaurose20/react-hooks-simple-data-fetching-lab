65b87f2505b9e2dc95fb9963f93dce691f84badd
rosekamau <kamaurose236@gmail.com>
2024-08-08 14:35:30 +0300

    react-hooks-simple-data-fetching-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
+const { useEffect, useState } = require("react")
+
 // create your App component here
+function App(){
+
+    const [image,setImage] = useState("")
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res=>res.json())
+        .then(data=>setImage(data.message));
+
+    },[])
+
+    return(
+        <div>
+            {!image && <p>Loading...</p>}
+            {image && <img src={image} alt="A Random Dog"/>}
+
+        </div>
+
+
+    )
+}
+export default App
